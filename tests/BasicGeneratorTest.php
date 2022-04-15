364a91156abb0057c283315d4ee730edf2c510ed
Dan <self@danpock.me>
2022-04-15 18:09:19 -0400

    Add test snapshot package and basic test

--- tests/BasicGeneratorTest.php
+++ tests/BasicGeneratorTest.php
@@ -6,19 +6,38 @@ namespace RobThree\Tests\UrlGenerator;
 
 use PHPUnit\Framework\TestCase;
 use RobThree\UrlGenerator\UrlGenerator;
+use Spatie\Snapshots\MatchesSnapshots;
 
 /**
  * @see UrlGenerator
  */
 class BasicGeneratorTest extends TestCase
 {
+    use MatchesSnapshots;
+
+    /**
+     * @var UrlGenerator
+     */
+    private $generator;
+
     private $defaultWordSets = [
         'adjectives' => ['big', 'funny', 'lazy'],
         'colors' => ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
         'animals' => ['dog', 'cat', 'hamster']
     ];
 
-    public function testTestVectors() {
-        //TODO: Implement
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->generator = new UrlGenerator($this->defaultWordSets);
+    }
+
+    public function testCanGenerateTheFirstTwoDozenIds() {
+        $firstTwoDozenIds =[];
+        for ($i = 0; $i <= 24; $i++) {
+            $firstTwoDozenIds[] = $this->generator->toURL($i);
+        }
+
+        $this->assertMatchesJsonSnapshot($firstTwoDozenIds);
     }
 }
