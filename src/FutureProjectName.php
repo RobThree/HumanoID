8ff43c7d6d5724a7624a26bcd9d970e638f0d4a2
Dan <self@danpock.me>
2022-04-18 13:35:18 -0400

    Ensure static builders can accept seperator and format

--- src/FutureProjectName.php
+++ src/FutureProjectName.php
@@ -5,23 +5,29 @@ namespace RobThree\UrlGenerator;
 // This is literally a placeholder name for w/e we end up picking the new name as.
 final class FutureProjectName
 {
-    public static function zooIdGenerator(): UrlGenerator
+    public static function zooIdGenerator(?string $separator = '-', $format = null): UrlGenerator
     {
         return new UrlGenerator(
             json_decode(
                 file_get_contents(__DIR__ . '/../data/zoo-words.json'),
                 true
-            )
+            ),
+            null,
+            $separator,
+            $format
         );
     }
 
-    public static function spaceIdGenerator(): UrlGenerator
+    public static function spaceIdGenerator(?string $separator = '-', $format = null): UrlGenerator
     {
         return new UrlGenerator(
             json_decode(
                 file_get_contents(__DIR__ . '/../data/space-words.json'),
                 true
-            )
+            ),
+            null,
+            $separator,
+            $format
         );
     }
 }
