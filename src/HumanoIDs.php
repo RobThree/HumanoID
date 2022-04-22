060e3c3c45d6a514ace724a646c5fb75a0c71cd5
RobThree <rob@devcorner.nl>
2022-04-22 23:20:45 +0200

    Fix for #10 case 2

--- src/HumanoIDs.php
+++ src/HumanoIDs.php
@@ -6,26 +6,32 @@ namespace RobThree\HumanoID;
 
 final class HumanoIDs
 {
+    private static HumanoID $zooGenerator;
+    private static HumanoID $spaceGenerator;
+
     public static function zooIdGenerator(
         ?string $separator = '-',
         ?WordFormatOption $format = null
     ): HumanoID {
-        return new HumanoID(
-            json_decode(
-                file_get_contents(__DIR__ . '/../data/zoo-words.json'),
-                true
-            ),
-            null,
-            $separator,
-            $format
-        );
+        if (self::$zooGenerator === null)
+            self::$zooGenerator = new HumanoID(
+                json_decode(
+                    file_get_contents(__DIR__ . '/../data/zoo-words.json'),
+                    true
+                ),
+                null,
+                $separator,
+                $format
+            );
+        return self::$zooGenerator;
     }
 
     public static function spaceIdGenerator(
         ?string $separator = '-',
         ?WordFormatOption $format = null
     ): HumanoID {
-        return new HumanoID(
+        if (self::$spaceGenerator === null)
+            self::$spaceGenerator = new HumanoID(
             json_decode(
                 file_get_contents(__DIR__ . '/../data/space-words.json'),
                 true
@@ -34,5 +40,6 @@ final class HumanoIDs
             $separator,
             $format
         );
+        return self::$spaceGenerator;
     }
 }
