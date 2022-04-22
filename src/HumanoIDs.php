14f6ccaa69d0b99cdad6649108f14dd460bb371d
Rob Janssen <rob@devcorner.nl>
2022-04-23 00:53:45 +0200

    Ensure single instance of generators in HumanoIDs class (#11)
    * Fix for #10 case 2

--- src/HumanoIDs.php
+++ src/HumanoIDs.php
@@ -6,33 +6,42 @@ namespace RobThree\HumanoID;
 
 final class HumanoIDs
 {
+    private static ?HumanoID $zooGenerator = null;
+    private static ?HumanoID $spaceGenerator = null;
+
     public static function zooIdGenerator(
         ?string $separator = '-',
         ?WordFormatOption $format = null
     ): HumanoID {
-        return new HumanoID(
-            json_decode(
-                file_get_contents(__DIR__ . '/../data/zoo-words.json'),
-                true
-            ),
-            null,
-            $separator,
-            $format
-        );
+        if (self::$zooGenerator === null) {
+            self::$zooGenerator = new HumanoID(
+                json_decode(
+                    file_get_contents(__DIR__ . '/../data/zoo-words.json'),
+                    true
+                ),
+                null,
+                $separator,
+                $format
+            );
+        }
+        return self::$zooGenerator;
     }
 
     public static function spaceIdGenerator(
         ?string $separator = '-',
         ?WordFormatOption $format = null
     ): HumanoID {
-        return new HumanoID(
-            json_decode(
-                file_get_contents(__DIR__ . '/../data/space-words.json'),
-                true
-            ),
-            null,
-            $separator,
-            $format
-        );
+        if (self::$spaceGenerator === null) {
+            self::$spaceGenerator = new HumanoID(
+                json_decode(
+                    file_get_contents(__DIR__ . '/../data/space-words.json'),
+                    true
+                ),
+                null,
+                $separator,
+                $format
+            );
+        }
+        return self::$spaceGenerator;
     }
 }
